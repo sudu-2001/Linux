10224dc9d8818e318e7713e26e99b71a86c565fe
sudarshan <kosudarshan15@gmail.com>
2024-09-22 18:57:29 +0530

    l

---
+++ database.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+password='Sudu@2001'
+
+for db in $(cat databases.txt); do
+
+	echo "Backup of databases: $db"
+
+	mysqldump -u root -p"$password" --databases "$db" > "${db}.sql"
+
+	if [ $? -eq 0 ]; then
+
+		echo "Backup of $db is succesfully completed."
+
+	else
+
+		echo "Backup of $db is not successfully completed."
+
+	fi
+
+	sleep 2
+
+done
